533ed029a84c119ae08ecfbcf95c31aa2d9332dc
erweiw <erwei.wang@amd.com>
2025-11-13 08:42:49 -0800

    New set of results including min, max and avg speeds

--- programming_examples/matrix_multiplication/i8/matmul_config_sweep.py
+++ programming_examples/matrix_multiplication/i8/matmul_config_sweep.py
@@ -13,7 +13,7 @@ from pathlib import Path
 from itertools import product
 from ml_dtypes import bfloat16
 
-def batched(iterable):
+def batched(iterable, existing = None):
     batch = []
     for row in iterable:
         # Extract values for row
@@ -25,10 +25,15 @@ def batched(iterable):
         tile_k_l1 = row[5]
         tile_n = row[6]
 
+        key = (m, k, n, tile_m, tile_k_l2, tile_k_l1, tile_n)
+        if key in existing and is_valid(existing[key]):
+            continue
+
         # Ensure m and n fit into tiles
-        if tile_m % 16 != 0: continue
+        if tile_m % 32 != 0: continue
         if tile_k_l1 % 8 != 0: continue
-        if tile_n % 16 != 0: continue
+        if tile_n % 32 != 0: continue
+        if tile_k_l2 % 32 != 0: continue
         if m % (tile_m * HERD_M) != 0: continue
         if n % (tile_n * HERD_N) != 0: continue
         if k % tile_k_l2 != 0: continue
@@ -36,7 +41,7 @@ def batched(iterable):
         # Ensure sufficient L1 space
         if (4*tile_m*tile_k_l1 + 4*tile_n*tile_k_l1 + 2*tile_m*tile_n) + 1024 > 2**16: continue
         # Ensure sufficient L2 space
-        if 2*(tile_m*tile_k_l2 + tile_n*tile_k_l2 + tile_m*tile_n) > 2**21: continue
+        if 2*(8*tile_m*tile_k_l2 + 4*tile_n*tile_k_l2 + 8*4*tile_m*tile_n) > 8*(2**19): continue
 
         yield row
 
@@ -48,33 +53,38 @@ def worker_chunk(batch):
             ["make", "compile-kernel", f"TILE_M={tile_m}", f"TILE_N={tile_n}", f"TILE_K_L1={tile_k_l1}", f"AIE_TARGET=aie2p"],
             cwd="..",
             capture_output=True,
-            text=True
+            text=True,
+            timeout=600*1000
         )
         output = subprocess.run(
             ["python", "../run.py", "--herd-m", str(8), "--herd-n", str(4),  "--m", str(m), "--k", str(k), "--n", str(n), "--tile-m", str(tile_m), "--tile-k-l2", str(tile_k_l2), "--tile-k-l1", str(tile_k_l1), "--tile-n", str(tile_n), "--compile-mode", "compile-only", "--arch", "aie2p"],
             capture_output=True,
-            text=True
+            text=True,
+            timeout=600*1000
         )
         output = subprocess.run([
             "./test.exe",
             "-x", "air.xclbin",
             "-k", "MLIR_AIE",
-            "-i" "air.insts.bin",
+            "-i", "air.insts.bin",
             "-M", str(m),
             "-K", str(k),
             "-N", str(n)
-        ], capture_output=True)
+        ], capture_output=True, timeout=600*1000)
         latency = re.findall("(?<=Avg NPU matmul time: )\\d+\\.\\d+", output.stdout.decode("utf-8"))
-        lat = float(latency[0])
+        lat_avg = float(latency[0])
+        latency = re.findall("(?<=Max NPU matmul time: )\\d+", output.stdout.decode("utf-8"))
+        lat_max = float(latency[0])
+        latency = re.findall("(?<=Min NPU matmul time: )\\d+", output.stdout.decode("utf-8"))
+        lat_min = float(latency[0])
 
     except Exception:
         print(traceback.format_exc())
-        lat = 0.0
+        lat_avg = 0.0
+        lat_max = 0.0
+        lat_min = 0.0
 
-    print(f"Latency: {lat}")
-    print()
-
-    return [m, k, n, tile_m, tile_k_l2, tile_k_l1, tile_n, lat]
+    return [m, k, n, tile_m, tile_k_l2, tile_k_l1, tile_n, lat_avg, lat_max, lat_min]
 
 def run_chunk(worker, chunk_iter):
     for chunk in chunk_iter:
@@ -101,20 +111,32 @@ def main():
     tile_k_l1_vals = find_factors(SIZE)
     tile_n_vals = find_factors(SIZE // HERD_N)
 
+    # Load in all csv values
+    existing = {}
+    if READ_FILE != None:
+        read_path = Path(READ_FILE)
+        if Path.exists(read_path):
+            with read_path.open(newline="", encoding="utf-8") as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    key = (int(row["m"]), int(row["k"]), int(row["n"]), int(row["tile_m"]), int(row["tile_k_l2"]), int(row["tile_k_l1"]), int(row["tile_n"]))
+                    existing[key] = row  # whole dict row
+
     with open(WRITE_FILE, 'w') as f:
         writer = csv.writer(f)
         writer.writerow([
             'm', 'k', 'n', 'tile_m', 'tile_k_l2', 'tile_k_l1', "tile_n",
-            'runner latency'
+            'latency avg', 'latency max', 'latency min'
         ])
-        chunk_iter = batched(product([SIZE], [SIZE], [SIZE], tile_m_vals, tile_k_l2_vals, tile_k_l1_vals, tile_n_vals))
+        chunk_iter = batched(product([SIZE], [SIZE], [SIZE], tile_m_vals, tile_k_l2_vals, tile_k_l1_vals, tile_n_vals), existing)
         for row in run_chunk(worker_chunk, chunk_iter):
             writer.writerow(row)
             f.flush()
 
-WRITE_FILE = "data1.csv"
+WRITE_FILE = "data1024_i8_rerun.csv"
+READ_FILE = None
 SIZE = 1024
-HERD_M = 8
+HERD_M = 4
 HERD_N = 4
 if __name__ == "__main__":
     main()
